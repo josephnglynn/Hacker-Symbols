d06ca0ea70ffe06445ed1fe38c27a13b4a0d07c3
Joseph <56293291+josephnglynn@users.noreply.github.com>
2021-04-11 12:20:49 +0100

    Moved To CPP As Not Compiling With C

--- main.cpp
+++ main.cpp
@@ -2,39 +2,39 @@
 // Created by joseph on 11/04/2021.
 //
 
+#define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <iostream>
 
 int main() {
-    GLFWwindow *window;
 
-    if (!glewInit()) {
+    if (!glfwInit()) {
         return -1;
     }
 
-    window = glfwCreateWindow(500, 500, "Hacker Symbols", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(640, 480, "Hacker Symbols", nullptr, nullptr);
 
     if (!window) {
         glfwTerminate();
         return -1;
     }
 
-
     glfwMakeContextCurrent(window);
 
     if (glewInit() != GLEW_OK) {
+        std::cout << "ERROR GLEW DIDN'T INIT" << std::endl;
         glfwTerminate();
         return -1;
     }
 
-    while (glfwWindowShouldClose(window)) {
+    std::cout << glGetString(GL_VERSION) << std::endl;
 
-
+    while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
     }
 
     glfwTerminate();
-
     return 0;
 }
